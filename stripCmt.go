7c71fabd27a518477f5e8c86b9bfe91053b294e6
ole108 <ole@bulbuk.de>
2015-07-24 17:57:51 +0200

    stripCmt starts to form itself. :-)

--- stripCmt.go
+++ stripCmt.go
@@ -1,5 +1,96 @@
 package main
 
+import (
+  "io"
+  "bufio"
+  "strings"
+)
+
+type LineReader interface {
+  ReadLine() (line string, err error)
+}
+
+type BufferedLineReader struct {
+  br *bufio.Reader
+}
+func NewBufferedLineReader(r io.Reader) LineReader {
+  return BufferedLineReader{bufio.NewReader(r)}
+}
+func (lr BufferedLineReader) ReadLine() (line string, err error) {
+  return lr.br.ReadString('\n')
+}
+
+type EolStripper struct {
+  lr LineReader
+}
+func NewEolStripper(lr LineReader) EolStripper {
+  return EolStripper{lr}
+}
+func (eols EolStripper) ReadLine() (line string, err error) {
+  line, err = eols.lr.ReadLine()
+  if err == nil || err == io.EOF {
+    line = strings.TrimRight(line, "\r\n")
+  }
+  return line, err
+}
+
+type EofDelayer struct {
+  lr LineReader
+  isEof bool
+}
+func NewEofDelayer(lr LineReader) *EofDelayer {
+  return &EofDelayer{lr, false}
+}
+func (eofd *EofDelayer) ReadLine() (line string, err error) {
+  if eofd.isEof {
+    return "", io.EOF
+  } else {
+    line, err = eofd.lr.ReadLine()
+    if err == io.EOF && len(line) > 0 {
+      eofd.isEof = true
+      return line, nil
+    }
+    return line, err
+  }
+}
+
+type LineCommentStripper struct {
+  lr LineReader
+}
+func NewLineCommentStripper(lr LineReader) LineCommentStripper {
+  return LineCommentStripper{lr}
+}
+func (lcs LineCommentStripper) ReadLine() (line string, err error) {
+  line, err = lcs.lr.ReadLine()
+  if err == nil || err == io.EOF {
+    idx := strings.Index(line, "//")
+    if idx >= 0 {
+      line = line[0:idx]
+    }
+  }
+  return line, err
+}
+
+
+/*
+type BlockCommentStripper struct {
+  lr LineReader
+  inComment bool
+}
+func NewBlockCommentStripper(lr LineReader) CommentStripper {
+  return BlockCommentStripper{lr, false}
+}
+
+type EndOfLineTrimmer struct {
+  cs CommentStripper
+}
+
+type EmptyLineStripper struct {
+  eolt EndOfLineTrimmer
+  emptyLineReturned bool
+}
+*/
+
 type Cell struct {
 	state bool
 }
--- stripCmt_test.go
+++ stripCmt_test.go
@@ -2,8 +2,107 @@ package main
 
 import (
 	"testing"
+	"io"
+	"strings"
 //	"fmt"
 )
+
+const foo = "foo"
+const fooN = "foo\n"
+const bar = "bar"
+
+func TestLineReader0(t *testing.T) {
+  lr := NewBufferedLineReader(strings.NewReader(""))
+  expectEof(t, lr)
+}
+func TestLineReader1(t *testing.T) {
+  lr := NewBufferedLineReader(strings.NewReader(fooN))
+  expectOneLine(t, lr, fooN)
+}
+func TestLineReader2(t *testing.T) {
+  lr := NewBufferedLineReader(strings.NewReader(strings.Join([]string{foo, bar}, "\n")))
+  expectTwoLinesEof(t, lr, fooN, bar)
+}
+
+func TestEolStripper0(t *testing.T) {
+  eols := NewEolStripper(NewBufferedLineReader(strings.NewReader(foo)))
+  expectEofLine(t, eols, 1, foo)
+}
+func TestEolStripperN(t *testing.T) {
+  eols := NewEolStripper(NewBufferedLineReader(strings.NewReader(fooN)))
+  expectOneLine(t, eols, foo)
+}
+func TestEolStripperR(t *testing.T) {
+  eols := NewEolStripper(NewBufferedLineReader(strings.NewReader(strings.Join([]string{foo, "\r"}, ""))))
+  expectEofLine(t, eols, 1, foo)
+}
+func TestEolStripperRN(t *testing.T) {
+  eols := NewEolStripper(NewBufferedLineReader(strings.NewReader(strings.Join([]string{foo, "\r\n"}, ""))))
+  expectOneLine(t, eols, foo)
+}
+
+func TestEofDelayerNoDelay(t *testing.T) {
+  lcs := NewEofDelayer(NewBufferedLineReader(strings.NewReader(fooN)))
+  expectOneLine(t, lcs, fooN)
+}
+func TestEofDelayerDelay(t *testing.T) {
+  lcs := NewEofDelayer(NewBufferedLineReader(strings.NewReader(foo)))
+  expectOneLine(t, lcs, foo)
+}
+
+func TestLineCommentStripperNoComment(t *testing.T) {
+  lcs := NewLineCommentStripper(NewEofDelayer(NewEolStripper(NewBufferedLineReader(strings.NewReader(fooN)))))
+  expectOneLine(t, lcs, foo)
+}
+func TestLineCommentStripperHalfComment(t *testing.T) {
+  lcs := NewLineCommentStripper(NewEofDelayer(NewEolStripper(NewBufferedLineReader(strings.NewReader(strings.Join([]string{foo, "// blue\n"}, ""))))))
+  expectOneLine(t, lcs, foo)
+}
+func TestLineCommentStripperFullComment(t *testing.T) {
+  lcs := NewLineCommentStripper(NewEofDelayer(NewEolStripper(NewBufferedLineReader(strings.NewReader("// blue")))))
+  expectOneLine(t, lcs, "")
+}
+
+
+func expectOneLine(t *testing.T, lr LineReader, expected string) {
+  expectLine(t, lr, 1, expected)
+  expectEof(t, lr)
+}
+func expectTwoLines(t *testing.T, lr LineReader, expected1 string, expected2 string) {
+  expectLine(t, lr, 1, expected1)
+  expectLine(t, lr, 2, expected2)
+  expectEof(t, lr)
+}
+func expectTwoLinesEof(t *testing.T, lr LineReader, expected1 string, expected2 string) {
+  expectLine(t, lr, 1, expected1)
+  expectEofLine(t, lr, 2, expected2)
+}
+func expectLine(t *testing.T, lr LineReader, lineNum int, expected string) {
+  line, err := lr.ReadLine()
+  if err != nil {
+    t.Fatal("ERROR: Unexpected error for line (", lineNum, "): ", err.Error())
+  }
+  if line != expected {
+    t.Error("ERROR: Unexpected line (", lineNum, "): '", line, "' (expected is '", expected, "').")
+  }
+}
+func expectEofLine(t *testing.T, lr LineReader, lineNum int, expected string) {
+  line, err := lr.ReadLine()
+  if err != io.EOF {
+    t.Error("ERROR: Expected EOF but got: ", err)
+  }
+  if line != expected {
+    t.Error("ERROR: Unexpected line (", lineNum, "): '", line, "' (expected is '", expected, "').")
+  }
+}
+func expectEof(t *testing.T, lr LineReader) {
+  _, err := lr.ReadLine()
+  if err != io.EOF {
+    t.Fatal("ERROR: Expected EOF but got: ", err)
+  }
+}
+
+
 
 func TestCellIsAlive(t *testing.T) {
 	cell := NewCell()
